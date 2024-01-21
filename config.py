52083ec5e4ee6e71df274df5a16064320d951648
oyethomassir <157202299+oyethomassir@users.noreply.github.com>
2024-01-22 00:31:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/ll4st_opII",
+    "https://github.com/TeamAloneOp/AloneXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
